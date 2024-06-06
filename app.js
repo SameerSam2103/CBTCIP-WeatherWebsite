d38b49f009c4d8f68719a9f5183524f6606901f8
SameerSam2103 <156094262+SameerSam2103@users.noreply.github.com>
2024-06-06 16:22:22 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+const apiKey = `72d525d9c68616bcfff49ca3d2e00c78`;
+;
+
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+  
+const url = (city)=> `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${`72d525d9c68616bcfff49ca3d2e00c78`}`;
+
+
+async function getWeatherByLocation(city){
+     
+         const resp = await fetch(url(city), {
+             origin: "cros" });
+         const respData = await resp.json();
+     
+           addWeatherToPage(respData);
+          
+     }
+
+      function addWeatherToPage(data){
+          const temp = Ktoc(data.main.temp);
+
+          const weather = document.createElement('div')
+          weather.classList.add('weather');
+
+          weather.innerHTML = `
+          <h2><img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /> ${temp}°C <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /></h2>
+          <small>${data.weather[0].main}</small>
+          
+          `;
+
+
+
+          main.innerHTML= "";
+           main.appendChild(weather);
+      };
+
+
+     function Ktoc(K){
+         return Math.floor(K - 273.15);
+     }
+
+
+
+     form.addEventListener('submit',(e) =>{
+        e.preventDefault();
+
+        const city = search.value;
+
+        if(city){
+            getWeatherByLocation(city)
+        }
+
+     });
